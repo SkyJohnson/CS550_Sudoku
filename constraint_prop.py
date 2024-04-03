c9b356092dc4aa935b9a81956e87816dfd84afda
Efrain Vasques <efrainmiguel@live.com>
2024-04-02 23:26:41 -0700

    Slight reformatting.

--- constraint_prop.py
+++ constraint_prop.py
@@ -3,8 +3,9 @@ Constraint propagation
 '''
 from queue import Queue
 def AC3(csp, queue=None, removals=None):
-    """AC3 constraint propagation
-    
+    """
+    AC3 constraint propagation
+
     """
     
     # Hints:
@@ -21,9 +22,10 @@ def AC3(csp, queue=None, removals=None):
     for x1 in csp.variables:
         for x2 in csp.neighbors[x1]:
             queue.put((x1, x2))
+            
+            
     # while queue not empty:
     while not queue.empty():
-        
     #   (x1, x2) = dequeue
         (x1, x2) = queue.get()
     #   if revise(csp, x1, x2):
@@ -34,10 +36,11 @@ def AC3(csp, queue=None, removals=None):
     #       else:
             else:
     #           for x in {neighbors(x1)-x2}:
-                for x in (csp.neighbors[x1] - {x2}): # I think this is fixed. But the final "solved" puzzle is the same as the initial puzzle.
+                for x in (csp.neighbors[x1] - {x2}): # I think this is fixed. But the final "solved" puzzle is the same as the initial puzzle or empty depending on the revise function.
                     queue.put((x, x1))
     # return true
     return True
+
 
 def revise(csp, x1, x2): # This is the pruning function/method.
     revised = False
@@ -49,7 +52,7 @@ def revise(csp, x1, x2): # This is the pruning function/method.
     #   if no y in domain(x2) satsfies constraint(x,y):
     # #       domain(x1).remove(x)
             for y in csp.domains[x2]:
-                if not y in csp.choices(x1) and not csp.constraints(x1, x, x2, y): # I think this is the part that's messing up.
+                if y in csp.choices(x1) and not csp.constraints(x1, x, x2, y): # I think this is the part that's messing up.
                     csp.prune(x1, x, None) # None is for removals.
     # #             revised = true
                     revised = True
